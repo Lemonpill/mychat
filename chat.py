de96c89fd1424dd285f6685f8c10c44f9b7cbe81
Valeri <code.lemonpill@gmail.com>
2026-02-01 20:29:26 +0200

    minimal openai chat loop

---
+++ chat.py
@@ -0,0 +1,54 @@
+from enum import StrEnum
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+END_WORD = "end"
+
+
+class OpenAIModel:
+    gpt_4o_mini = "gpt-4o-mini"
+
+
+class OpenAIChat:
+    def __init__(self, client: OpenAI):
+        self.client: OpenAI = client
+        self.converstation: list[dict] = []
+        self.model: OpenAIModel | None = None
+
+    def new(self, model: OpenAIModel):
+        print(f"OpenAIChat.new: model={model}")
+        self.converstation = []
+        self.model = model
+
+    def say(self, thing: str):
+        print(f"OpenAIChat.say: thing={thing}")
+        if self.model is None:
+            raise RuntimeWarning("must define a model")
+        user_message = {"role": "user", "content": thing}
+        self.converstation.append(user_message)
+        resp = self.client.chat.completions.create(model=self.model, messages=self.converstation)
+        resp_text = resp.choices[0].message.content
+        chat_message = {"role": "assistant", "content": resp_text}
+        self.converstation.append(chat_message)
+        return resp_text
+
+    def end(self):
+        print("OpenAIChat.end")
+
+
+def main():
+    load_dotenv()
+    chat = OpenAIChat(OpenAI())
+    chat.new(model=OpenAIModel.gpt_4o_mini)
+    while True:
+        thing = input("user said:\n")
+        if thing == END_WORD:
+            chat.end()
+            break
+        compl = chat.say(thing)
+        print(f"chat said:\n{compl}")
+
+
+if __name__ == "__main__":
+    main()
